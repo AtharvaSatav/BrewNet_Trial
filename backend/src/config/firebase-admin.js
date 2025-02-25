357c4a6fd59115e0431cda4bbfa7c0315c708f90
AtharvaSatav <satavaa21.extc@coeptech.ac.in>
2025-02-25 17:15:39 +0530

    changes + deployfiles 2

--- backend/src/config/firebase-admin.js
+++ backend/src/config/firebase-admin.js
@@ -1,8 +1,23 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+
+// Use environment variables instead of serviceAccountKey.json
+const firebaseConfig = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL
+};
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(firebaseConfig)
 });
 
 module.exports = admin;
+
+// const admin = require('firebase-admin');
+// const serviceAccount = require('./serviceAccountKey.json');
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
+
+// module.exports = admin;
